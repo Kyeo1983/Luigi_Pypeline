125b0d636041114987aa68b0fac7f07b4a53a558
Kyeo1983 <kyeo_ses@yahoo.com>
2018-08-19 00:39:29 +0800

    Changed open w to open w+ to create file if its not there

--- job_factory.py
+++ job_factory.py
@@ -34,8 +34,8 @@ def render(tpl_path, context):
     path, filename = os.path.split(tpl_path)
     return Environment(loader=FileSystemLoader(path or './')).get_template(filename).render(context)
 
-output = open(outputfile, 'w')
-outputbat = open(outputbatfile, 'w')
+output = open(outputfile, 'w+')
+outputbat = open(outputbatfile, 'w+')
 
 
 to_import = []
