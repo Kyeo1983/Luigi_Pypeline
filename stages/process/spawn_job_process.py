34bb9b78acf9640640b8c6126f830fb774fc1b20
Kyeo1983 <kyeo_ses@yahoo.com>
2018-08-22 19:54:18 +0800

    corected linebreak

--- stages/process/spawn_job_process.py
+++ stages/process/spawn_job_process.py
@@ -10,7 +10,8 @@ class {{job}}_{{id}}(luigi.Task):
     def scanJobTable():
         dbcol = { 'JOBID':0, 'JOBNAME':1, 'UNLOADED':2, 'LOAD_DAY':3, 'LOAD_TIME':4, 'UNLOAD_TIME':5, 'FREQ':6, 'NEXT_RUN_TIME':7 }
         
-        df = pd.read_csv('../temp/jobtable.csv')now = datetime.now()
+        df = pd.read_csv('../temp/jobtable.csv')
+        now = datetime.now()
         for row in df.values:
             loaded_ok = row[dbcol.UNLOADED] != 'Y'
 
