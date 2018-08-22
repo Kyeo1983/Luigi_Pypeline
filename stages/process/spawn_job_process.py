19150fa72bd10d5c0b09cb50a02c6f46f74be89e
Kyeo1983 <kyeo_ses@yahoo.com>
2018-08-22 11:50:00 +0800

    Test spawner stage

---
+++ stages/process/spawn_job_process.py
@@ -0,0 +1,13 @@
+from subprocess import Popen
+
+class {{job}}_{{id}}(luigi.Task):    
+    def requires(self):
+        return {{job}}_{{parent}}()
+    
+    def run(self):
+        proc = Popen(['{{param_jobname}}'])
+        with open(self.output().path, 'w') as out:
+            out.write(str('running'))
+
+    def output(self):
+        return luigi.LocalTarget(str(ctx['sysFolder']) + '/run/{{id}}.mrk')
