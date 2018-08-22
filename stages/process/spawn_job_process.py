ee8a6ce4de85876945371f0bb987e27bfeb376b1
Kyeo1983 <kyeo_ses@yahoo.com>
2018-08-22 20:05:19 +0800

    corrected dict indexing

--- stages/process/spawn_job_process.py
+++ stages/process/spawn_job_process.py
@@ -12,27 +12,27 @@ class {{job}}_{{id}}(luigi.Task):
 
 
         def scanJobTable():
-            dbcol = { JOBID:0, JOBNAME:1, UNLOADED:2, LOAD_DAY:3, LOAD_TIME:4, UNLOAD_TIME:5, FREQ:6, NEXT_RUN_TIME:7 }
+            dbcol = { 'JOBID':0, 'JOBNAME':1, 'UNLOADED':2, 'LOAD_DAY':3, 'LOAD_TIME':4, 'UNLOAD_TIME':5, 'FREQ':6, 'NEXT_RUN_TIME':7 }
 
             df = pd.read_csv('../temp/jobtable.csv')
             now = datetime.now()
             for row in df.values:
-                loaded_ok = row[dbcol.UNLOADED] != 'Y'
+                loaded_ok = row[dbcol['UNLOADED']] != 'Y'
 
-                load_time = datetime.strptime(row[dbcol.LOAD_TIME], '%H:%M').time()
-                unload_time = datetime.strptime(row[dbcol.UNLOAD_TIME], '%H:%M').time()
+                load_time = datetime.strptime(row[dbcol['LOAD_TIME']], '%H:%M').time()
+                unload_time = datetime.strptime(row[dbcol['UNLOAD_TIME']], '%H:%M').time()
                 time_ok = load_time <= now.time() and now.time() <= unload_time
 
-                load_days = row[dbcol.LOAD_DAY].split(',')
+                load_days = row[dbcol['LOAD_DAY']].split(',')
                 day_ok = str(now.weekday() + 1) in load_days
 
-                next_run_ok = row[dbcol.NEXT_RUN_TIME] is None or math.isnan(row[dbcol.NEXT_RUN_TIME])
+                next_run_ok = row[dbcol['NEXT_RUN_TIME']] is None or math.isnan(row[dbcol['NEXT_RUN_TIME']])
                 if not next_run_ok:
-                    next_run_time = datetime.strptime(row[dbcol.NEXT_RUN_TIME], '%H:%M').time()
+                    next_run_time = datetime.strptime(row[dbcol['NEXT_RUN_TIME']], '%H:%M').time()
                     next_run_ok = next_run_time.time() <= now.time()
 
                 if loaded_ok and time_ok and day_ok and next_run_ok:
-                    runJob(row[dbcol.JOBNAME], row[dbcol.FREQ])
+                    runJob(row[dbcol['JOBNAME']], row[dbcol['FREQ']])
 
 
         def runJob(jobname, freq):
