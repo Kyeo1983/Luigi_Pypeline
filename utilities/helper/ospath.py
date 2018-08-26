575b32b4898ea5ea21784cac063dec717a319f35
Kyeo1983 <kyeo_ses@yahoo.com>
2018-08-26 18:17:02 +0800

    sample autodoc

--- utilities/helper/ospath.py
+++ utilities/helper/ospath.py
@@ -1,8 +1,21 @@
 import os
 
+"""
+Module relating to OS Pathing.
+"""
 class OSPath:
+    """
+    Internal class to handle paths according to Mac / Windows.
+    """
     @classmethod
     def path(self, p):
+        """
+        Given a PathLib's Path object, returns a resolved format depending on calling OS is Mac or Windows.        
+        
+        :param p: pathlib's Path object representing an OS path.
+        :type p: pathlib.Path
+        :returns: string
+        """
         if os.name == 'nt':
             # on windows
             return str(p)
