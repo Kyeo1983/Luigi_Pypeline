393da31caf5116e16804edd34cd5cf14a080605f
Kyeo1983 <kyeo_ses@yahoo.com>
2019-03-16 00:50:17 +0800

    Redirect spawner sequence from /inputs folder

--- spawn_factory.py
+++ spawn_factory.py
@@ -22,7 +22,7 @@ from configs.importsconf import conf as importsconfig
 from configs.stagesconf import conf as stagesconfig
 
 
-input_file = json.load(open('./temp/spawner_seq.json', 'r'))
+input_file = json.load(open('./inputs/spawner_seq.json', 'r'))
 job_name = input_file['name']
 input_steps = input_file['sequence']
 
@@ -36,7 +36,7 @@ def render(tpl_path, context):
     path, filename = os.path.split(tpl_path)
     return Environment(loader=FileSystemLoader(path or './')).get_template(filename).render(context)
 
-    
+
 output = open(OSPath.path(outputfile), 'w')
 outputbat = open(OSPath.path(outputbatfile), 'w')
 output.write("".join(open(OSPath.path(BASE_STAGE_PATH / 'imports.py'), "r").readlines()) + "\n")
@@ -55,15 +55,15 @@ rendered.append(render(OSPath.path(BASE_STAGE_PATH / 'start.py'), { "job": job_n
 for step in input_steps:
     if 'parent' not in step:
         step['parent'] = 'start'
-    
+
     step['job'] = job_name
     stageconf = stagesconfig[step['stage']]
     ren = render(stageconf['path'], step)
     rendered.append(ren + '\n')
-    
+
     # getting needed imports into list
     to_import = to_import + [i for i in stageconf['dependencies'] if i not in to_import]
-    
+
     # adding the ending
     list_job_id.append(step['id'])
     list_parent_id.append(step['parent'])
@@ -74,8 +74,8 @@ end_stg = { 'job': job_name, 'stage': 'end', 'parent': ','.join(list_leaf_stagen
 
 ren = render(OSPath.path(BASE_STAGE_PATH / 'end.py'), end_stg)
 rendered.append('{}\n'.format(ren))
-    
-    
+
+
 # looping through import list and write import lines
 for imports in to_import:
     importconf = importsconfig[imports]
@@ -84,11 +84,11 @@ for imports in to_import:
         importstr = 'from {} import {}'.format(importconf['package'], importconf['importclass'])
     else:
         importstr = 'import {}'.format(importconf['package'])
-    
+
     if 'alias' in importconf:
-        importstr = '{} as {}'.format(importstr, importconf['alias'])        
+        importstr = '{} as {}'.format(importstr, importconf['alias'])
     output.write('{}\n'.format(importstr))
-    
+
 
 # Writing Folder to Script
 sysFolder = OSPath.path(JOB_MARKER_PATH / job_name)
@@ -101,8 +101,8 @@ output.write("ctx['sysJobName'] = '{}'\n".format(job_name))
 
 for item in rendered:
     output.write("{}\n".format(item))
-    
-    
+
+
 # prep bat file
 outputbat.write('python -m luigi --module {} {}_end\n'.format(job_name, job_name))
 outputbat.close()
