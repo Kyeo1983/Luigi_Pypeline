c8c5a13d3031404de7f93bc257c6bf52753febc8
Kyeo1983 <kyeoses@kyeo-vm-us-east1-1.us-east1-b.c.kyeo-home.internal>
2019-03-23 01:36:29 +0000

    Updated to instantiate db no matter what

--- init/setup.py
+++ init/setup.py
@@ -11,16 +11,13 @@ from configs.appconf import conf
 # Get path of db
 scheduler_db_path = conf["scheduler_db_path"]
 
-
-# Create DB if it doesn't exist
-if not os.path.isfile(scheduler_db_path):
-    # When connect to an SQLite database file that does not exist, SQLite creates a new database
-    conn = sqlite3.connect(scheduler_db_path)
-    # Create main table
-    sql_create_table = """CREATE TABLE IF NOT EXISTS JOBS
-        (jobid INTEGER PRIMARY KEY, jobname TEXT NOT NULL, UNLOADED TEXT,
-        LOAD_DAY TEXT, LOAD_TIME INTEGER, UNLOAD_TIME INTEGER, FREQ TEXT
-        , ISADHOC TEXT, HOLD TEXT
-        );"""
-    c = conn.cursor()
-    c.execute(sql_create_table)
+# When connect to an SQLite database file that does not exist, SQLite creates a new database
+conn = sqlite3.connect(scheduler_db_path)
+# Create main table
+sql_create_table = """CREATE TABLE IF NOT EXISTS JOBS
+    (jobid INTEGER PRIMARY KEY, jobname TEXT NOT NULL, UNLOADED TEXT,
+    LOAD_DAY TEXT, LOAD_TIME INTEGER, UNLOAD_TIME INTEGER, FREQ TEXT
+    , ISADHOC TEXT, HOLD TEXT
+    );"""
+c = conn.cursor()
+c.execute(sql_create_table)
