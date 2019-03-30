b920b16d028efcd98bb0561e3b717789963755a4
Kyeo1983 <kyeo_ses@yahoo.com>
2019-03-30 18:37:39 +0800

    Fix syntax

--- stages/operations/email.py
+++ stages/operations/email.py
@@ -19,8 +19,7 @@ class {{job}}_{{id}}(luigi.Task):
         emailconf = email()
         smtpconf = smtp()
 
-        cmd = 'echo "{}" | s-nail -s "{}" -r "{}<{}>" -S smtp="{}:{}" -S smtp-use-starttls -S smtp-auth-login -S smtp-auth-user="{}" -S smtp-auth-password="{}" -S ssl-verify=ignore {}'
-        .format('{{param_in_body}}', '{{param_in_title}}', emailconf.sendername, emailconf.sender, smtpconf.host, smtpconf.port, smtpconf.username, smtpconf.password, '{{param_in_recipient}}')
+        cmd = 'echo "{}" | s-nail -s "{}" -r "{}<{}>" -S smtp="{}:{}" -S smtp-use-starttls -S smtp-auth-login -S smtp-auth-user="{}" -S smtp-auth-password="{}" -S ssl-verify=ignore {}'.format('{{param_in_body}}', '{{param_in_title}}', emailconf.sendername, emailconf.sender, smtpconf.host, smtpconf.port, smtpconf.username, smtpconf.password, '{{param_in_recipient}}')
         logger.info(cmd)
         subprocess.call(cmd, shell=True)
 
