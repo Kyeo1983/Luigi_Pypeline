3d733652b983ec6a54ec28b395cbe6fcb254ae15
Kyeo1983 <kyeo_ses@yahoo.com>
2019-03-31 00:14:17 +0800

    Try TEXT for time data type

--- init/setup.py
+++ init/setup.py
@@ -16,7 +16,7 @@ conn = sqlite3.connect(scheduler_db_path)
 # Create main table
 sql_create_table = """CREATE TABLE IF NOT EXISTS JOBS
     (jobid INTEGER PRIMARY KEY, jobname TEXT NOT NULL, UNLOADED TEXT,
-    LOAD_DAY TEXT, LOAD_TIME INTEGER, UNLOAD_TIME INTEGER, FREQ TEXT
+    LOAD_DAY TEXT, LOAD_TIME TEXT, UNLOAD_TIME TEXT, FREQ TEXT
     , ISADHOC TEXT, HOLD TEXT
     );"""
 c = conn.cursor()
