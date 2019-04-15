c831c67d91b9801714faa6b61fcf2f5b71f0cd59
Kyeo1983 <kyeo_ses@yahoo.com>
2019-04-15 08:40:12 +0800

    Corrected filepath

--- jobs/custom_suumo_scrape.sh
+++ jobs/custom_suumo_scrape.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -f -R /home/kyeoses/pypeline/jobs/jobmarkers/my_test_job/run/*
-cp -f -R /home/kyeoses/pypeline/jobs/jobmarkers/my_test_job/run/save /home/kyeoses/pypeline/jobs/jobmarkers/custom_suumo_scrape/run/*
+rm -f -R /home/kyeoses/pypeline/jobs/jobmarkers/custom_suumo_scrape/run/*
+cp -f -R /home/kyeoses/pypeline/jobs/jobmarkers/custom_suumo_scrape/run/save /home/kyeoses/pypeline/jobs/jobmarkers/custom_suumo_scrape/run/*
 python -m luigi --module custom_suumo_scrape custom_suumo_scrape_end
