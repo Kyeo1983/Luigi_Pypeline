e3fe95128efb454989f005c69fdb2abff11f9b34
Kyeo1983 <kyeoses@kyeo-vm-us-east1-1.us-east1-b.c.kyeo-home.internal>
2019-07-31 20:07:22 +0000

    added project_id

--- configs/appconf.py
+++ configs/appconf.py
@@ -3,5 +3,6 @@ App level configurations
 """
 conf = {
     "scheduler_db_path" : "../luigi_central_scheduler/schedule.db",
-    "working_dir" : "/kyeo/home/pypeline"
+    "working_dir" : "/kyeo/home/pypeline",
+    "project_id" : "kyeo-home"
 }
