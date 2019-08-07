e8936806818a3c464d3761c0bfea948a36e11d45
Kyeo1983 <kyeoses@kyeo-vm-us-east1-1.us-east1-b.c.kyeo-home.internal>
2019-08-07 20:31:32 +0000

    changed access control

--- configs/appconf.py
+++ configs/appconf.py
@@ -3,6 +3,6 @@ App level configurations
 """
 conf = {
     "scheduler_db_path" : "../luigi_central_scheduler/schedule.db",
-    "working_dir" : "/kyeo/home/pypeline",
+    "working_dir" : "/home/kyeoses/pypeline",
     "project_id" : "kyeo-home"
 }
