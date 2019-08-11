b91d024e7f08226cf8a1b80aac53b45a564c9778
Kyeo1983 <lumoscreator_gmail_com@kyeo-vm-us-east1-1.us-east1-b.c.kyeo-home.internal>
2019-08-11 09:12:10 +0000

    Added chmod in .sh to share out log file

--- jobs/sample_stage.sh
+++ jobs/sample_stage.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 rm -f -R /home/kyeoses/pypeline/jobs/jobmarkers/sample_stage/run/*
 python -m luigi --module sample_stage sample_stage_end
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi.log
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi-server.log
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi-stages.log
