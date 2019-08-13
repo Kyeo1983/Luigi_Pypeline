c80b46d0d8e06dcca4c4c5feff5c661fa97fe3cc
Yuting <42912700+yuuuuu0801@users.noreply.github.com>
2019-08-13 18:23:09 +0800

    Add files via upload

--- jobs/custom_pbsa_unite.sh
+++ jobs/custom_pbsa_unite.sh
@@ -3,3 +3,7 @@ export GOOGLE_APPLICATION_CREDENTIALS=/home/kyeoses/pypeline/keys/compute_engine
 #rm -f -R /home/kyeoses/pypeline/jobs/jobmarkers/custom_officee_scrape/run/*
 #cp -f -R /home/kyeoses/pypeline/jobs/jobmarkers/custom_officee_scrape/run/save /home/kyeoses/pypeline/jobs/jobmarkers/custom_officee_scrape/run/*
 python -m luigi --module custom_pbsa_unite custom_pbsa_unite_end
+
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi.log
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi-server.log
+chmod 777 /home/kyeoses/pypeline/var/logs/luigi/luigi-stages.log
